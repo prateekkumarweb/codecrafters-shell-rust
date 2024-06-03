77baf2c2a9746aba3c3d1d9dd11f23fa91cd542c
Prateek Kumar <me@prateek.page>
2024-06-03 20:48:59 +0530

    echo command

--- src/main.rs
+++ src/main.rs
@@ -9,13 +9,27 @@ fn main() {
         input.clear();
         stdin.read_line(&mut input).unwrap();
         let trimmed = input.trim();
-        if trimmed.starts_with("exit") {
-            let code = trimmed.splitn(2, " ");
-            let code = code.last().unwrap().parse::<i32>().unwrap();
-            std::process::exit(code);
-        }
-        if !trimmed.is_empty() {
-            println!("{}: command not found", trimmed)
+        let mut splits = trimmed
+            .split(' ')
+            .map(|s| s.trim())
+            .filter(|x| !x.is_empty());
+        let command = splits.next();
+        let Some(command) = command else {
+            continue;
         };
+        match command {
+            "exit" => {
+                let code = splits.next().unwrap();
+                let code = code.parse::<i32>().unwrap();
+                std::process::exit(code);
+            }
+            "echo" => {
+                let args = splits.collect::<Vec<&str>>().join(" ");
+                println!("{}", args);
+            }
+            command => {
+                println!("{}: command not found", command)
+            }
+        }
     }
 }
