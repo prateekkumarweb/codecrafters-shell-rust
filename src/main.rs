e333e0bb3167582343698f66ef69d3625ef13b58
Prateek Kumar <me@prateek.page>
2024-06-03 21:09:33 +0530

    type for executables

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,26 @@
-use std::io::{self, Write};
+use std::{
+    collections::HashMap,
+    fs,
+    io::{self, Write},
+};
 
 fn main() {
     let stdin = io::stdin();
     let mut input = String::new();
+
+    let path_env = std::env::var("PATH").unwrap_or("".to_string());
+    let paths = path_env.split(':');
+
+    let execs = paths
+        .map(fs::read_dir)
+        .filter_map(|dir| dir.ok())
+        .flat_map(|dir| {
+            dir.filter_map(|p| p.ok())
+                .filter(|p| p.path().is_file())
+                .filter_map(|p| p.file_name().to_str().map(|f| (f.to_owned(), p.path())))
+        })
+        .collect::<HashMap<_, _>>();
+
     loop {
         print!("$ ");
         io::stdout().flush().unwrap();
@@ -32,6 +50,8 @@ fn main() {
                 let command = splits.next().unwrap();
                 if builtins.contains(&command) {
                     println!("{} is a shell builtin", command);
+                } else if let Some(p) = execs.get(command) {
+                    println!("{} is {}", command, p.as_os_str().to_str().unwrap());
                 } else {
                     println!("{} not found", command);
                 }
